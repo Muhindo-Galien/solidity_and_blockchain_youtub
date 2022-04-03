7724c545999dd8b99e680068f3a74876cebf2372
Muhindo-Galien <muhindogalien512@gmail.com>
2022-04-03 12:41:53 -0700

    brownie web3 simpple storage done

---
+++ demos/brownie_simple_storage/contracts/SimpleStorage.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >0.6.0 <0.9.0;
+
+contract SimpleStorage {
+    uint256 public favoriteNumber;
+
+    struct People {
+        string name;
+        uint256 favoriteNumber;
+    }
+    mapping(string => uint256) public nameTofavoriteNumber;
+    People[] public people;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function addPeople(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_name, _favoriteNumber));
+        nameTofavoriteNumber[_name] = _favoriteNumber;
+    }
+}
