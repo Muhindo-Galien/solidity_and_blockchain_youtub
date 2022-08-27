5d934aab1bd1681f18aed5a59073a9063a020530
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2022-08-27 09:07:11 -0700

    Update lesson3FundMe.sol

--- lesson3FundMe.sol
+++ lesson3FundMe.sol
@@ -4,7 +4,6 @@ pragma solidity >=0.6.0 <0.9.0;
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
-
 contract FundMe {
     using SafeMathChainlink for uint256;
     mapping(address => uint256) public addressToAmountFunded;
